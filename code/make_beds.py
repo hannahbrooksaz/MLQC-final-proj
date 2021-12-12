16c1f67a26536eb134a7494255014d2ba23324ac
Jakob Weiss <jakobweiss@jakobs-mbp.bc.edu>
2021-12-12 10:24:27 -0500

    added datasets

---
+++ code/make_beds.py
@@ -0,0 +1,58 @@
+import re
+
+###
+dataset_path = './canonical_dataset.txt'
+###
+
+def makeBeds(which):
+    
+    if which == 'train':
+        suffix = 'train'
+        CHROM_GROUP = ['chr11', 'chr13', 'chr15', 'chr17', 'chr19', 'chr21',
+                       'chr2', 'chr4', 'chr6', 'chr8', 'chr10', 'chr12',
+                       'chr14', 'chr16', 'chr18', 'chr20', 'chr22', 'chrX', 'chrY']
+    elif which == 'test':
+        suffix = 'test'
+        CHROM_GROUP = ['chr1', 'chr3', 'chr5', 'chr7', 'chr9']
+    else:
+        suffix = 'all'
+        CHROM_GROUP = ['chr1', 'chr3', 'chr5', 'chr7', 'chr9',
+                       'chr11', 'chr13', 'chr15', 'chr17', 'chr19', 'chr21',
+                       'chr2', 'chr4', 'chr6', 'chr8', 'chr10', 'chr12',
+                       'chr14', 'chr16', 'chr18', 'chr20', 'chr22', 'chrX', 'chrY']
+        
+    roi_out = open('roi_'+suffix+'.bed', 'w')
+    sites_out = open('sites_'+suffix+'.bed', 'w')
+
+    with open(dataset_path) as f:
+        for line in f.readlines():
+            data = re.split('\n|\t', line)
+
+            if (data[2] in CHROM_GROUP and data[1] == '1'):
+                roi_out.write(data[2]+'\t'+data[4]+'\t'+data[5]+'\n')
+
+                for start_coord_set in data[6::2]:
+                    if len(start_coord_set) > 0:
+                        for start_coord in start_coord_set.split(','):
+                            if len(start_coord) > 0:
+                                sites_out.write(data[2]+'\t'+
+                                                start_coord+'\t'+
+                                                start_coord+'\t'+
+                                                '.\t1\t.\n')
+
+                for end_coord_set in data[7::2]:
+                    if len(end_coord_set) > 0:
+                        for end_coord in end_coord_set.split(','):
+                            if len(end_coord) > 0:
+                                sites_out.write(data[2]+'\t'+
+                                                end_coord+'\t'+
+                                                end_coord+'\t'+
+                                                '.\t-1\t.\n')
+        
+    roi_out.close()
+    sites_out.close()
+
+    print('done with '+suffix)
+
+makeBeds('train')
+makeBeds('test')
